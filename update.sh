a3960a3b3625e95d66b85510137dff6b8ff6473d
Knut Ahlers <knut@ahlers.me>
2019-06-29 16:24:51 +0200

    Initial version
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

---
+++ update.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -euxo pipefail
+
+# Create a tempdir to operate in
+tempdir=$(mktemp -d)
+
+# Ensure tempdir is removed on exit
+function cleanup() {
+	rm -rf ${tempdir}
+}
+trap cleanup EXIT
+
+# Check input
+PKG=${1:-}
+[[ -n ${PKG} ]] || {
+	echo "No package given as first argument" >&2
+	exit 1
+}
+
+# Initialize git directory
+cd ${tempdir}
+git init
+
+# Configure user details
+git config user.email "jenkins@luzifer.io"
+git config user.name "Luzifer.io Jenkins"
+
+# Add AUR as remote
+git remote add origin "git@github.com:luzifer-aur/${PKG}.git"
+
+# Get latest state of remote
+git fetch --all --tags
+
+# Reset to latest master
+git reset --hard origin/master
+git branch -u origin/master
+
+# Check for update script
+[ -f update_version.sh ] || {
+	echo "No update_version.sh found, skipping build."
+	exit 0
+}
+
+# Execute update script
+docker run --rm -i -u $(id -u) \
+	-v "$(pwd):$(pwd)" -w "$(pwd)" \
+	luzifer/aur-update \
+	bash ./update_version.sh
+
+# Check for new commits
+(git status --porcelain -b | grep -q '^## .*ahead') || {
+	echo "No update-commits were made."
+	exit 0
+}
+
+# Check whether the build is possible
+docker run --rm -i \
+	-v "$(pwd):/src" \
+	luzifer/arch-repo-builder:latest
+
+# Push changes including tags to fork
+git push origin master
